a969c000a69cb4dd2a76144da483a11444ba47cf
Paras Mahajan <parasmahajan1010@gmail.com>
2025-11-24 09:48:55 +0530

    MY FIRST EVER C++ CODE

---
+++ day1/hello.cpp
@@ -0,0 +1,7 @@
+#include "std_lib_facilities.h"
+int main()    //C++ programs start by executing the function main
+{
+	
+    std::cout << "Hello, World!\n";    // output "Hello, World!"
+    return 0;
+}    
