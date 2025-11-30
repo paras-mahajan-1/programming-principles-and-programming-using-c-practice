f33e9bb0e26f152704a19f4cddca438518f76bac
Paras Mahajan <parasmahajan1010@gmail.com>
2025-11-30 09:38:39 +0530

    Chapter 3 unpto 3.3 complete section

---
+++ day3/thinkingOfError.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+int main() 
+{
+    //int number_of_steps = 39;
+    //string name = "Paras";
+    //cout << number_of_steps << " " << name << "\n";
+    //int number = "Paras";
+    //string name = 39;
+    //cout << number << " " << name << "\n";
+    int number_of_steps = 39;
+    double flying_time = 3.5;
+    char decimal_point = '.';
+    string name = "Annem";
+    bool tap_on = true;
+    cout << number_of_steps << " " << flying_time << " " << decimal_point << " " << name << " " << tap_on << "\n";
+    return 0;
+
+}
